00e10fdd7a1b594e62db29383443db564eb6e7f5
BodartA <a.bodart23@gmail.com>
2024-04-10 08:31:03 +0200

    Minors changes and responsive navbar work, I just wanna add few modifications.

--- src/assets/components/Navbar.tsx
+++ src/assets/components/Navbar.tsx
@@ -14,27 +14,27 @@ const Navbar = () => {
     return (
         <div className=' bg-yellow-400 shadow-md w-full fixed left-0 top-0'>
 
-            <div className='font-mono text-zinc-800 md:px-10 py-4 px-7 md:flex justify-between items-center'>
+            <div className='font-mono text-zinc-800 md:px-5 py-4 px-3 md:flex justify-between items-center'>
 
                 <div className='flex text-4xl items-center gap-2'>
                     <img src={codexImage} className='w-10 h-10'></img>
                     <span className=' font-bold'>Xeno Codex</span>
                 </div>
 
-                <div onClick={() => setIsOpen(!isOpen)} className='w-7 h-7 absolute right-8 top-4 cursor-pointer md:hidden'>
+                <div onClick={() => setIsOpen(!isOpen)} className='w-7 h-7 absolute right-4 top-4 cursor-pointer md:hidden'>
                     {
                         isOpen ? <XMarkIcon /> : <Bars3BottomRightIcon />
                     }
                 </div>
 
-                <ul className={`md:flex pl-9 md:pl-0 md:items-center ${isOpen ? '' : ' transition-colors text-red-500'} `}>
+                <ul className={`md:flex md:px-0 px-3 md:pl-0 md:items-center ${isOpen ? '' : 'hidden'}`}>
                     {
                         Links.map(link => (
                             <li className=' font-semibold my-5 md:my-0 md:ml-8'>
                                 <a href='/'>{link.name}</a>
                             </li>))
                     }
-                    <button className='btn bg-zinc-800 text-yellow-400 py-1 px-3 md:ml-8 rounded md:static'>Mon Compte</button>
+                    <button className='btn bg-zinc-800 text-yellow-400 py-1 px-3 md:ml-8 rounded md:static'>Connexion</button>
                 </ul>
 
             </div>
