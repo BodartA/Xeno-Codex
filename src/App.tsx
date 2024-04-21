89ca682c51af1f7326861d48191743fc78f08407
BodartA <a.bodart23@gmail.com>
2024-04-21 18:09:13 +0200

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import Homepage from './assets/components/Homepage'
-import Navbar from './assets/components/Navbar'
+import Homepage from './assets/components/pages/Homepage'
+// import Navbar from './assets/components/Navbar'
 import Terminides from './assets/components/pages/Terminides'
 import Automatons from './assets/components/pages/Automatons'
 import LoginPage from './assets/components/pages/LoginPage'
 
 function App() {
   return (
-      <div className='bg-zinc-800 min-h-screen w-full'>
-        <Navbar />
-        <div>
-          <Routes>
-            <Route path='/home' element={<Homepage />}></Route>
-            <Route path='/terminides' element={<Terminides />}></Route>
-            <Route path='/automatons' element={<Automatons />}></Route>
-            <Route path='/login' element={<LoginPage />}></Route>
-          </Routes>
-        </div>
-      </div>
+    <>
+    <div className='bg-zinc-800 h-screen w-full' >
+      <div className='bg-yellow-400 h-1/2 absolute w-full -skew-y-2 -mt-20'></div>
+      <Homepage />
+    </div>
+    </>
+      
   )
 }
 
---
+++ src/assets/components/pages/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Homepage = () => {
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default Homepage;
