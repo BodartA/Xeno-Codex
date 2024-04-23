7510f32640929275d24db8b7e2d0b6703c85db29
BodartA <a.bodart23@gmail.com>
2024-04-23 16:33:15 +0200

    Minor style change

--- src/assets/components/XenoCards.tsx
+++ src/assets/components/XenoCards.tsx
@@ -5,7 +5,7 @@ const XenoCards = () => {
   return (
     <>
       <div className='flex m-4'>
-        <div className='h-72 w-64 rounded-lg bg-purple-800 absolute z-0'></div>
+        <div className='h-72 w-64 rounded-lg bg-gradient-to-tl from-purple-950 to-purple-800 absolute z-0'></div>
         <div className='h-72 w-64 rounded-lg border-2 border-zinc-800 bg-yellow-400 text-center content-center z-10 -translate-x-1.5 -translate-y-1.5 hover:translate-x-0 hover:translate-y-0 duration-150'>
           <h2 className='font-mono font-bold uppercase text-zinc-800 text-4xl'>Titan</h2>
           <img src={titanImage} className='object-center w-full object-contain p-2'></img>
