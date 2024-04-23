405b7466bee72fa78b63535bb01f0dd052a9290f
BodartA <a.bodart23@gmail.com>
2024-04-23 14:48:57 +0200

    Minor changes

--- src/assets/components/Navbar.tsx
+++ src/assets/components/Navbar.tsx
@@ -3,10 +3,10 @@ import { AdjustmentsHorizontalIcon, MagnifyingGlassCircleIcon } from '@heroicons
 const Navbar = () => {
 
   return (
-    <div className='flex mx-auto h-14 w-1/3 bg-white'>
-      <input className="p-1 bg-transparent w-full" type='text' placeholder='Entrez votre menace ...' />
-      <AdjustmentsHorizontalIcon className='p-1' />
-      <MagnifyingGlassCircleIcon className='p-1' />
+    <div className='flex mx-auto h-14 w-1/3 bg-white rounded-full'>
+      <input className=" focus:outline-none pl-4 bg-transparent w-full font-mono" type='text' placeholder='Entrez votre menace ...' />
+      <AdjustmentsHorizontalIcon className='' />
+      <MagnifyingGlassCircleIcon className='' />
     </div>
   );
 };
--- src/assets/components/pages/Header.tsx
+++ src/assets/components/pages/Header.tsx
@@ -3,9 +3,9 @@ import codexImage from '../../images/codex.png'
 
 const Header = () => {
   return (
-    <div className='h-40 justify-center flex'>
+    <div className='h-40 justify-center flex drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]'>
       <img className=' mt-auto h-12 w-12 mx-4 my-2' src={codexImage}></img>
-      <h1 className=" mt-auto uppercase text-6xl font-bold font font-roboto text-yellow-400">Xeno Codex</h1>
+      <h1 className=" mt-auto uppercase text-6xl font-bold font font-roboto text-yellow-400 ">Xeno Codex</h1>
     </div>
   );
 };
