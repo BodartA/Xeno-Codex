0923c2035d173310c55cfcb3b7dd0463aebc65db
BodartA <a.bodart23@gmail.com>
2024-05-05 18:00:46 +0200

    Global design changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import Main from './assets/components/pages/Main'
 function App() {
   return (
     <>
-      <div className=' bg-gradient-to-b from-zinc-800 to-yellow-400 min-w-full h-full min-h-screen'>
+      <div className=' bg-zinc-800 min-w-full h-full min-h-screen -z-10'>
+      <div className=' bg-yellow-400 -skew-y-2 h-3/5 min-w-full absolute top-0 left-0 -mt-56 z-0'></div>
         <div className='grid grid-cols-1 grid-row-3 gap-8'>
           <Header />
           <Navbar />
--- src/assets/components/Navbar.tsx
+++ src/assets/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 
 
   return (
-    <div className='flex mx-auto h-14 w-1/3 bg-white rounded-full'>
+    <div className='flex mx-auto h-14 w-1/3 bg-white rounded-full border-zinc-800 border-2 z-10'>
       <input className=" focus:outline-none pl-4 bg-transparent w-full font-mono" type='text' placeholder='Entrez votre menace ...'/>
       <AdjustmentsHorizontalIcon className='' />
       <MagnifyingGlassCircleIcon className='' />
