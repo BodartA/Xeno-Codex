eb672ed15c0db62142202e87ded281f5e490f7e7
BodartA <a.bodart23@gmail.com>
2024-05-06 07:51:38 +0200

    Start adding data from Firebase to my XenoCards

--- src/assets/components/XenosCards.tsx
+++ src/assets/components/XenosCards.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, getDocs, collection, doc, getDoc, where, query } from 'firebase/firestore';
 import titanImage from '../images/titan.png'
+
+const app = {
+  apiKey: "AIzaSyDsRTs6J_ibHCIYLYFahCRi1QxzH1frPzo",
+  authDomain: "xeno-codex.firebaseapp.com",
+  projectId: "xeno-codex",
+  storageBucket: "xeno-codex.appspot.com",
+  messagingSenderId: "761825618027",
+  appId: "1:761825618027:web:2c662cec99710a6588292e",
+  measurementId: "G-WQT0M73MTD"
+}
+
+const firebaseApp = initializeApp(app)
+const db = getFirestore(firebaseApp)
+
+const threats = {
+  name: "Bile Titan",
+  color: "from-purple-600 to-purple-400",
+  race: "Terminids",
+  photo: "titanImage"
+}
 
 const XenosCards = () => {
 
-  return(
+  return (
 
-    XenosCardsModel()
+    XenosCardsModel(threats.name, threats.color, threats.race, threats.photo)
   )
 }
 
-function XenosCardsModel() {
+function XenosCardsModel(name: string, color: string, race: string, photo: string) {
 
   return (
     <>
       <div className='grid m-4'>
-        <div className={'max-h-72 max-w-64 rounded-lg bg-gradient-to-tl from-red-600 to-red-400 col-start-1 row-start-1'}></div>
+        <div className={`max-h-72 max-w-64 rounded-lg bg-gradient-to-tl ${color} col-start-1 row-start-1`}/>
         <div className='max-h-72 max-w-64 rounded-lg border-2 border-zinc-800 bg-slate-50 text-center content-center z-10 -translate-x-1.5 -translate-y-1.5 hover:translate-x-0 hover:translate-y-0 duration-150 col-start-1 row-start-1'>
-          <h2 className='font-mono font-bold uppercase text-zinc-800 text-4xl'>Titan</h2>
+          <h2 className='font-mono font-bold uppercase text-zinc-800 text-4xl'>{name}</h2>
           <img src={titanImage} className='object-center w-full object-contain p-2'></img>
-          <h3 className='text-zinc-800 font-mono font-semibold'>Terminides</h3>
+          <h3 className='text-zinc-800 font-mono font-semibold'>{race}</h3>
         </div>
       </div>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { initializeApp } from 'firebase/app'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-
-export const firebaseApp = initializeApp({
-
-  apiKey: "AIzaSyDsRTs6J_ibHCIYLYFahCRi1QxzH1frPzo",
-  authDomain: "xeno-codex.firebaseapp.com",
-  projectId: "xeno-codex",
-  storageBucket: "xeno-codex.appspot.com",
-  messagingSenderId: "761825618027",
-  appId: "1:761825618027:web:2c662cec99710a6588292e",
-  measurementId: "G-WQT0M73MTD"
-});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
