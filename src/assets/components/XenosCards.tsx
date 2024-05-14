53bdfffbff5750979a435e062831ba6f0a97d9e5
BodartA <a.bodart23@gmail.com>
2024-05-14 16:49:02 +0200

    Datas added with a simple JSON File

--- src/assets/components/XenosCards.tsx
+++ src/assets/components/XenosCards.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import titanImage from '../images/titan.png'
 
-export function XenosCards() {
+export function XenosCards({ data }) {
 
   return (
     <>
+
+
+      {data.map((threat) => {
+
+        const {id, name, race, color, image} = threat
+
+        return (
           <div className='grid m-4'>
             <div className={`max-h-72 max-w-64 rounded-lg bg-red-600 col-start-1 row-start-1`} />
             <div className='max-h-72 max-w-64 rounded-lg border-2 border-zinc-800 bg-slate-50 text-center content-center z-10 -translate-x-1.5 -translate-y-1.5 hover:translate-x-0 hover:translate-y-0 duration-150 col-start-1 row-start-1'>
-              <h2 className='font-mono font-bold uppercase text-zinc-800 text-4xl'>Bile Titan</h2>
-              <img src={titanImage} alt={"Titan"} className='object-center w-full object-contain p-2'></img>
-              <h3 className='text-zinc-800 font-mono font-semibold'>Terminids</h3>
+              <h2 className='font-mono font-bold uppercase text-zinc-800 text-4xl'>{name}</h2>
+              <img src={image} alt={name} className='object-center w-full object-contain p-2'></img>
+              <h3 className='text-zinc-800 font-mono font-semibold'>{race}</h3>
             </div>
           </div>
+        )
+      })}
+
     </>
   )
 }
--- src/assets/components/pages/Main.tsx
+++ src/assets/components/pages/Main.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useState } from 'react';
+import data from '../../../data.json'
 import XenosCards from '../XenosCards';
 
 const Main = () => {
+
+  const [threat, setThreat] = useState(data)
 
   return (
     <>
       <div className='w-10/12 mx-auto z-10'>
         <div className='grid grid-cols-2 gap-1 sm:grid-cols-2 sm:gap-2 md:grid-cols-3 md:gap-3 lg:grid-cols-4 lg:gap-4 xl:grid-cols-5 xl:gap-6 2xl:grid-cols-6 2xl:gap-8'>
-          <XenosCards />
+          <XenosCards data={data} />
         </div>
       </div>
     </>
