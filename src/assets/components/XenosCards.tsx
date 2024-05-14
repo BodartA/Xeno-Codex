91b0207d070e40278e3da38c289cb6a953bb6926
BodartA <a.bodart23@gmail.com>
2024-05-14 15:58:52 +0200

    Clean Firebase stuff

--- src/assets/components/XenosCards.tsx
+++ src/assets/components/XenosCards.tsx
@@ -1,30 +1,18 @@
 import React from 'react';
 import titanImage from '../images/titan.png'
 
-export function XenosCards({ datas }) {
+export function XenosCards() {
 
   return (
     <>
-      {datas.map((threat) => {
-
-        const { id, name, race, color, photo } = threat;
-
-        console.log(threat)
-
-        return (
-
-          <div key={id} className='grid m-4'>
-            <div className={`max-h-72 max-w-64 rounded-lg bg-gradient-to-tl ${color} col-start-1 row-start-1`} />
+          <div className='grid m-4'>
+            <div className={`max-h-72 max-w-64 rounded-lg bg-red-600 col-start-1 row-start-1`} />
             <div className='max-h-72 max-w-64 rounded-lg border-2 border-zinc-800 bg-slate-50 text-center content-center z-10 -translate-x-1.5 -translate-y-1.5 hover:translate-x-0 hover:translate-y-0 duration-150 col-start-1 row-start-1'>
-              <h2 className='font-mono font-bold uppercase text-zinc-800 text-4xl'>{name}</h2>
-              <img src={photo} alt={photo} className='object-center w-full object-contain p-2'></img>
-              <h3 className='text-zinc-800 font-mono font-semibold'>{race}</h3>
+              <h2 className='font-mono font-bold uppercase text-zinc-800 text-4xl'>Bile Titan</h2>
+              <img src={titanImage} alt={"Titan"} className='object-center w-full object-contain p-2'></img>
+              <h3 className='text-zinc-800 font-mono font-semibold'>Terminids</h3>
             </div>
           </div>
-        )
-
-      })}
-
     </>
   )
 }
--- src/assets/components/pages/Main.tsx
+++ src/assets/components/pages/Main.tsx
@@ -1,29 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import { initializeApp } from 'firebase/app';
-import { getFirestore, getDocs, collection, doc, getDoc, query } from 'firebase/firestore';
 import XenosCards from '../XenosCards';
-
-const app = {
-  apiKey: "AIzaSyDsRTs6J_ibHCIYLYFahCRi1QxzH1frPzo",
-  authDomain: "xeno-codex.firebaseapp.com",
-  projectId: "xeno-codex",
-  storageBucket: "xeno-codex.appspot.com",
-  messagingSenderId: "761825618027",
-  appId: "1:761825618027:web:2c662cec99710a6588292e",
-  measurementId: "G-WQT0M73MTD"
-}
-
-const firebaseApp = initializeApp(app)
-const db = getFirestore(firebaseApp)
-
-let datas = []
-
-const allThreatsDatas = await getDocs(collection(db, "threats"));
-allThreatsDatas.forEach((doc) => {
-  datas = [doc.id, doc.data()]
-  console.log(datas)
-})
 
 const Main = () => {
 
@@ -31,7 +8,7 @@ const Main = () => {
     <>
       <div className='w-10/12 mx-auto z-10'>
         <div className='grid grid-cols-2 gap-1 sm:grid-cols-2 sm:gap-2 md:grid-cols-3 md:gap-3 lg:grid-cols-4 lg:gap-4 xl:grid-cols-5 xl:gap-6 2xl:grid-cols-6 2xl:gap-8'>
-          <XenosCards datas={datas} />
+          <XenosCards />
         </div>
       </div>
     </>
